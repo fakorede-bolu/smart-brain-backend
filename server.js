5db63ee80bfb3e6fbdbf57f5df5967ee551a97d9
Watife <watife14@yahoo.com>
2018-06-18 09:52:53 +0100

    updating db

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'boluwatife',
-        database: 'smart_brain_db'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
